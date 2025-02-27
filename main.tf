9af604340371f9f743ca11df6f2b60cd2d0cf83b
glennbech <glenn.bech@gmail.com>
2025-02-27 09:56:58 +0100

    fix

--- main.tf
+++ main.tf
@@ -6,17 +6,16 @@ resource "aws_s3_bucket" "storage_bucket" {
   bucket = "my-company-storage-bucket"
 }
 
-resource "aws_s3_bucket_lifecycle_rule" "storage_bucket_lifecycle" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration#noncurrent_version_expiration
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
   bucket = aws_s3_bucket.storage_bucket.id
-  id      = "archive_rule"
-  enabled = true
 
-  transition {
-    days          = 90
-    storage_class = "GLACIER"
-  }
-
-  expiration {
-    days = 365
+  rule {
+    id = "Allow small object transitions"
+    status = "Enabled"
+    transition {
+      days          = 365
+      storage_class = "GLACIER_IR"
+    }
   }
 }
